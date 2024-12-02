531d0cd30b49370a0417897f996a69298eef3361
Shihab Hasan <shihabhasan.iut@gmail.com>
2024-12-02 11:47:36 -0600

    acceptance tests

--- tests/acceptanceTests/test_GameOver.py
+++ tests/acceptanceTests/test_GameOver.py
@@ -265,15 +265,6 @@ class TestGameOver:
         game_manager.place_piece(0,6)
         game_manager.end_turn()
 
-        # And it's white's turn
-        assert game_manager.get_turn() == Color.WHITE
-
-        # When none of the white pieces can move
-        assert len(game_manager.get_movable_options(0,0)) == 0
-        assert len(game_manager.get_movable_options(3,0)) == 0
-        assert len(game_manager.get_movable_options(0,3)) == 0
-        assert len(game_manager.get_movable_options(1,1)) == 0
-
         # history file yet not created
         file_path = history_path + 'game_history_' + game_manager.id + '.json'
         assert not os.path.exists(file_path)
@@ -284,10 +275,28 @@ class TestGameOver:
         # And the black piece player has won
         assert result == Color.BLACK
 
-        # history file should exists now
+        # history file should exist now
         assert os.path.exists(file_path)
 
         # deleting history file
         game_manager.delete_history_file(history_path)
+        assert not os.path.exists(file_path)
+
+    def test_no_history_file_for_incomplete_game(self, setup_board):
+        # Given an ongoing game with a complete piece placement phase
+        game_manager = setup_board
+        game_manager.place_piece(0,0)
+        game_manager.place_piece(3,0)
+        game_manager.place_piece(0,3)
+        game_manager.place_piece(1,1)
+        game_manager.end_turn()
+        game_manager.place_piece(1,3)
+        game_manager.place_piece(3,1)
+        game_manager.place_piece(6,0)
+        game_manager.place_piece(0,6)
+        game_manager.end_turn()
+
+        # history file yet not created
+        file_path = history_path + 'game_history_' + game_manager.id + '.json'
         assert not os.path.exists(file_path)
 
